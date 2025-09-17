1bf3b208eb871d488cc2708fe32babc69bdbc8a2
cimisachimi <mfaturrahmann@gmail.com>
2025-09-17 08:39:13 +0700

    idk

--- config/cors.php
+++ config/cors.php
@@ -1,22 +1,12 @@
+// config/cors.php
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    */
-
 'paths' => ['api/*', 'sanctum/csrf-cookie'],
 'allowed_methods' => ['*'],
-'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+'allowed_origins' => ['http://localhost:3000'],
 'allowed_headers' => ['*'],
 'supports_credentials' => true,
+
 
 ];
